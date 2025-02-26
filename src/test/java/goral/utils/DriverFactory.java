c4f24b545d506668b55647b628434db626a2fa50
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-02-26 21:22:28 +0100

    driver factory

---
+++ src/test/java/goral/utils/DriverFactory.java
@@ -0,0 +1,8 @@
+package goral.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+    WebDriver driver = new ChromeDriver();
+}
