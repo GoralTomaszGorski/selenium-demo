847176454e16478eed26259be1a6cfd00aed1e99
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-03-01 17:42:59 +0100

    add test when email same

--- src/test/java/goral/tests/RegisterTest.java
+++ src/test/java/goral/tests/RegisterTest.java
@@ -9,22 +9,31 @@ import goral.utils.DateUtil;
 
 public class RegisterTest extends BaseTest{
 
-
-    String ganerateEmail = "Tomek" + DateUtil.getCurrentDateTime()+ "@testowy.pl";
+    String generateEmail = "Tomek" + DateUtil.getCurrentDateTime()+ "@testowy.pl";
 
     @Test
     public void registerTest(){
         System.out.println("Mock");
         System.out.println(DateUtil.getCurrentDateTime());
+        System.out.println(DateUtil.generateRandomNumber(1, 100));
     }
 
     @Test
     public void registerUserTest(){
        WebElement dashboardLink =  new HomePage(driver).openMyAccountPage()
-               .registerUser(ganerateEmail, ganerateEmail).getDashboardLink();
+               .registerUser(generateEmail, generateEmail).getDashboardLink();
 
                Assert.assertTrue(dashboardLink.isDisplayed());
                Assert.assertEquals(dashboardLink.getText(), "Dashboard");  
     }
 
+
+    @Test
+    public void registerUserWithSameTest() {
+        WebElement error = new HomePage(driver).openMyAccountPage()
+                .registerUser("tomek@testowy.pl", "tomek@testowy.pl").getError();
+
+        Assert.assertTrue(error.getText().contains("Error: An account is already registered with your email address. Please log in."), "Dashboard");
+        System.out.println("berrotberrot");
+    }
 }
