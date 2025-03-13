569fff6828b8251468888bc660283dc12af24c69
Tomasz Gorski <t.gorski@posnet.com>
2025-03-13 11:18:48 +0100

    register test with yaml

--- src/test/java/goral/tests/RegisterTest.java
+++ src/test/java/goral/tests/RegisterTest.java
@@ -1,19 +1,26 @@
 package goral.tests;
 
 import goral.pages.HomePage;
+import goral.yamReader.YamlData;
+import goral.yamReader.YamlReader;
+
 import org.testng.annotations.Test;
 
-public class RegisterTest extends BaseTest{
+public class RegisterTest extends BaseTest {
+
+    YamlReader reader = new YamlReader();
+    YamlData dataYaml = reader.getYamlData();
 
     @Test
-    public void registerTest(){
+    public void registerTest() {
         System.out.println("Mock");
     }
 
     @Test
-    public void registerUserTest(){
-       new HomePage(driver).openMyAccountPage()
-               .registerUser("tester@testowy.pl", "tester@testowy.pl");
+    public void registerUserTest() {
+        new HomePage(driver)
+                .openMyAccountPage()
+                .registerUser((dataYaml.getUsername()), (dataYaml.getPassword()));
     }
 
 }
