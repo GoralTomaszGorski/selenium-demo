d71fb2196540ea56ce06cacd6abcd49c0f9f31b9
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-03-14 17:53:08 +0100

    add additional fields

--- src/test/java/goral/utils/yamReader/YamlData.java
+++ src/test/java/goral/utils/yamReader/YamlData.java
@@ -6,9 +6,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class YamlData {
-    private String username;
+    private String login;
     private String password;
     private String urlString;
+    private String userName;
+    private String userSurname;
+    private String street;
+    private String buildingNb;
+    private String postOffice;
+    private String postCode;
+    private String city;
 
     public YamlData() {
     }
