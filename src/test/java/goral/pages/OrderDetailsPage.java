cc14d6e97b88120ff78d06bfce0816b4349390cb
Tomasz Gorski <t.gorski@posnet.com>
2025-03-20 09:17:26 +0100

    product selector

--- src/test/java/goral/pages/OrderDetailsPage.java
+++ src/test/java/goral/pages/OrderDetailsPage.java
@@ -10,6 +10,9 @@ public class OrderDetailsPage {
     @FindBy( xpath = ("//div[@class='woocommerce-order']"))
     private WebElement orderNotice;
 
+    @FindBy( xpath = ("//td[contains (@class, 'product-name')]"))
+    private WebElement productName;
+
     public OrderDetailsPage(WebDriver driver){
         PageFactory.initElements(driver, this);
     }
@@ -17,6 +20,12 @@ public class OrderDetailsPage {
     public WebElement getOrderNotice() {
         return orderNotice;
     }
+
+    public WebElement getproductName() {
+        return productName;
+    }
+
+
 }
 
 
