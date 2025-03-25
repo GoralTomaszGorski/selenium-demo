17bccfb043f5c39abc17827748a642445a76e4ba
Tomasz Gorski <t.gorski@posnet.com>
2025-03-25 10:59:51 +0100

    reports

---
+++ src/test/java/goral/utils/SeleniumHelper.java
@@ -0,0 +1,51 @@
+package goral.utils;
+
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.model.Media;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class SeleniumHelper {
+
+    public static void waitForElementExist(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public static void waitForElementExist(WebDriver driver, WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static void waitForNotEmptyList(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1));
+        wait.until(browser -> browser.findElements(locator).size() > 0);
+    }
+
+    private static String takeScreenshot(WebDriver driver) throws IOException {
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+        String pathScreen = "src/test/resources/screenshoots/screen" + getCurrentTimestamp() + ".jpg";
+        FileUtils.copyFile(file, new File(pathScreen));
+        return pathScreen;
+    }
+
+    public static String getCurrentTimestamp() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss");
+        return LocalDateTime.now().format(formatter);
+    }
+
+
+    public static Media getScreenshot(WebDriver driver) throws IOException {
+        String path = takeScreenshot(driver);
+        return MediaEntityBuilder.createScreenCaptureFromPath(path).build();
+    }
+}
